a2c4ea8930f5afc9f50dfdee914b52e2c3a85caf
Alyssa <MasterWhisker@users.noreply.github.com>
2018-09-06 23:27:13 +1000

    minor design fix

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ function page_generate($template) {
 	} else if ($template === 'wrong') {
 		echo "		<form class='text-center' method='POST' action='" . htmlspecialchars($_SERVER['PHP_SELF']) . "'>\n";
 		echo "			<fieldset class='row'>\n";
-		echo "				<h3 style='color: red;'>Please correct your login details.</h3>";
+		echo "				<h3 style='color: red; text-align: center;;'>Please correct your login details.</h3>";
 		echo "				<div class='col-md-4 col-lg-4 col-xl-5 col-sm-2'></div>\n";
 		echo "				<div style='margin: 0 auto;' class='form-group col-xs-12 col-sm-8 col-md-4 col-lg-4 col-xl-2'>\n";
 		echo "					<label for='username'>Username: </label>\n";
@@ -36,18 +36,18 @@ function page_generate($template) {
 		echo "				<div class='col-md-4 col-lg-4 col-xl-5 col-sm-2'></div>\n";
 		echo "			</fieldset>\n";
 		echo "			<fieldset class='row'>\n";
-		echo "				<div class='col-md-2 col-lg-3 col-xl-3'></div>\n";
-		echo "				<div style='margin: 0 auto;' class='form-group col-xs-12 col-sm-12 col-md-8 col-lg-6 col-xl-6'>\n";
+		echo "				<div class='col-md-4 col-lg-4 col-xl-5 col-sm-2'></div>\n";
+		echo "				<div style='margin: 0 auto;' class='form-group col-xs-12 col-sm-8 col-md-4 col-lg-4 col-xl-2'>\n";
 		echo "					<label for='password'>Password: </label>\n";
 		echo "					<input class='form-control' type='text' name='password' id='password' placeholder='dictionary'>\n";
 		echo "				</div>\n";
-		echo "				<div class='col-md-2 col-lg-3 col-xl-3'></div>\n";
+		echo "				<div class='col-md-4 col-lg-4 col-xl-5 col-sm-2'></div>\n";
 		echo "			</fieldset>\n";
 		echo "			<span id='form-error' class='form-text'>Please enter the correct username and password.</span>\n";
 		echo "			<input style='margin-top: 1%;' type='submit' value='Login'>\n";
 		echo "		</form>\n";
 	} else if ($template === 'show') {
-		$con = mysqli_connect("localhost", "redacted", "redacted", "health_records");
+		$con = mysqli_connect("localhost", "redacted", "redacted", "redacted");
 		if (mysqli_connect_errno()) {
 			die("Failed to connect to MySQL: " . mysqli_connect_error());
 		}
@@ -123,12 +123,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		<link rel="stylesheet" href="css/bootswatch.css" media="screen">
 	</head>
 	<body style="margin: 0 auto; text-align: center;" class="container-fluid">
-		<a href="http://192.168.1.3"><img src="range_logo_transparent.png" alt="The Range Logo (Transparent Background)"></a>
+		<a href="#"><img src="range_logo_transparent.png" alt="The Range Logo (Transparent Background)"></a>
 		<h1>Health Records</h1>
 <?php page_generate($page_generate); ?>
 <footer style="position: fixed; bottom: 1px; left: 0; right: 0; height: 10%;">
 	<hr style="border: 0; height: 1px; background: #333; background-image: linear-gradient(to right, #ccc, #333, #ccc);">
-	<em style="width: 10%; float: right;">Designed Ally S</em>
+	<em style="width: 10%; float: right;">Designed by Ally S</em>
 	<em style="width: 10%; float: left;">Copyright 2018 The Range</em>
 </footer>
 	</body>
